87501ee5edc01498ab555eaf80f9f7ec40bf1f65
Kenny Baron <kennybaron@fastmail.com>
2024-12-03 19:27:30 -0500

    hacky prevent texture buffer overflow

--- src/triangle.c
+++ src/triangle.c
@@ -168,14 +168,10 @@ void draw_texel(
 	interpolated_v /= interpolated_reciprocal_w;
 
 	// Map the UV coord to full texture width and height
-	int tex_x = abs((int)(interpolated_u * texture_width));
-	int tex_y = abs((int)(interpolated_v * texture_height));
+	int tex_x = abs((int)(interpolated_u * texture_width)) % texture_width;
+	int tex_y = abs((int)(interpolated_v * texture_height)) % texture_height;
 
-	// todo test tex_x and tex_y to prevent buffer overflow
-	int i = (texture_width * tex_y) + tex_x;
-	if (i < texture_width * texture_height) {
-		draw_pixel(x, y, texture[(texture_width * tex_y) + tex_x]);
-	}
+	draw_pixel(x, y, texture[(texture_width * tex_y) + tex_x]);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
